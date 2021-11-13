a424cbc2f39647d0e716bf873664a3f374dc2ca6
DayvonAllen <dayvonallen93@outlook.com>
2021-11-13 13:45:45 -0600

    added notes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { useEffect } from "react";
+import "./App.css";
 function App() {
+  useEffect(() => {
+    const loadProvider = async () => {};
+
+    loadProvider();
+  }, []);
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <div className="balance-view is-size-2">
+            Current Balance: <strong>10</strong> ETH
+          </div>
+          <button
+            className="btn mr-2"
+            onClick={async () => {
+              const accounts = await window.ethereum.request({
+                method: "eth_requestAccounts",
+              });
+              console.log(accounts);
+            }}
+          >
+            Enable Ethereum
+          </button>
+          <button className="btn mr-2">Donate</button>
+          <button className="btn">Withdraw</button>
+        </div>
+      </div>
+    </>
   );
 }
 
